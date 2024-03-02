083645277491ba1ff830e0b4dd25a2292aaf1405
Roque-Miguelito-BSIT32E3 <shinrai150@gmail.com>
2024-03-02 15:35:03 +0800

    initial commit

---
+++ RELIM_E3_RoqueMiguelito_BSIT32E3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RELIM_E3_RoqueMiguelito_BSIT32E3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
